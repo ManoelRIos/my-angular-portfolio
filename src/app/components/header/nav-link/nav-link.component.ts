7c21fd60d53ae40c39e3f351449ef1f7c0ed31dd
MANOEL-DELLG15\manoe <informatica@apaebrasil.org.br>
2024-05-17 20:14:18 -0300

    release: Adicionando novo componente Nav Link

---
+++ src/app/components/header/nav-link/nav-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-link',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-link.component.html',
+  styleUrl: './nav-link.component.scss',
+})
+export class NavLinkComponent {
+  @Input() text: string = '';
+  @Input() link: string = '';
+
+  constructor() {}
+}
